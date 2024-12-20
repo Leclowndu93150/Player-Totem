d856e4c3f170680f06116f276e145c55594d520a
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-12-20 21:44:36 +0100

    oh my lord it works

--- src/main/java/com/leclowndu93150/playertotem/TotemItemRenderer.java
+++ src/main/java/com/leclowndu93150/playertotem/TotemItemRenderer.java
@@ -1,10 +1,11 @@
 package com.leclowndu93150.playertotem;
 
+import com.google.gson.Gson;
+import com.mojang.blaze3d.platform.NativeImage;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Font;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.EntityModelSet;
 import net.minecraft.client.model.geom.ModelLayers;
@@ -13,20 +14,28 @@ import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.texture.DynamicTexture;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
 public class TotemItemRenderer extends BlockEntityWithoutLevelRenderer {
+    private static final Gson GSON = new Gson();
+    private final Map<String, String> uuidCache = new HashMap<>();
 
     private PlayerModel<AbstractClientPlayer> playerModel;
-    private ResourceLocation customSkin;
+    private final Map<String, ResourceLocation> skinCache = new HashMap<>();
 
     public TotemItemRenderer() {
         super(Minecraft.getInstance().getBlockEntityRenderDispatcher(), Minecraft.getInstance().getEntityModels());
-        this.customSkin = null;
     }
 
     private void initializeModel() {
@@ -37,13 +46,64 @@ public class TotemItemRenderer extends BlockEntityWithoutLevelRenderer {
         }
     }
 
-    public void setCustomSkin(ResourceLocation skin) {
-        this.customSkin = skin;
+    private void loadSkinForName(String username) {
+        if (skinCache.containsKey(username)) {
+            return;
+        }
+
+        String uuid = uuidCache.computeIfAbsent(username, this::fetchUUIDFromAPI);
+        if (uuid == null || uuid.isEmpty()) {
+            skinCache.put(username, Minecraft.getInstance().player.getSkin().texture());
+            return;
+        }
+
+        CompletableFuture.runAsync(() -> {
+            try {
+                String profileUrl = String.format("https://sessionserver.mojang.com/session/minecraft/profile/%s", uuid);
+                String profileResponse = new java.util.Scanner(new URL(profileUrl).openStream()).useDelimiter("\\A").next();
+                MojangProfileResponse profileData = GSON.fromJson(profileResponse, MojangProfileResponse.class);
+
+                String skinUrl = profileData.getSkinURL();
+                NativeImage nativeImage = NativeImage.read(new URL(skinUrl).openStream());
+                DynamicTexture texture = new DynamicTexture(nativeImage);
+
+                ResourceLocation textureLocation = ResourceLocation.fromNamespaceAndPath("playertotem", "skin_" + username.toLowerCase());
+                Minecraft.getInstance().getTextureManager().register(textureLocation, texture);
+
+                skinCache.put(username, textureLocation);
+            } catch (Exception e) {
+                e.printStackTrace();
+                skinCache.put(username, Minecraft.getInstance().player.getSkin().texture());
+            }
+        });
+    }
+
+    private String fetchUUIDFromAPI(String username) {
+        try {
+            String uuidUrl = String.format("https://api.mojang.com/users/profiles/minecraft/%s", username);
+            String response = new java.util.Scanner(new URL(uuidUrl).openStream()).useDelimiter("\\A").next();
+            MojangUUIDResponse uuidData = GSON.fromJson(response, MojangUUIDResponse.class);
+            return uuidData.id;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
-    public void renderByItem(ItemStack stack, ItemDisplayContext displayContext, PoseStack poseStack, MultiBufferSource buffer, int combinedLight, int combinedOverlay) {
+    public void renderByItem(ItemStack stack, ItemDisplayContext displayContext, PoseStack poseStack,
+                             MultiBufferSource buffer, int combinedLight, int combinedOverlay) {
         initializeModel();
+
+        ResourceLocation skinLocation;
+        if (stack.has(DataComponents.CUSTOM_NAME)) {
+            String customName = stack.get(DataComponents.CUSTOM_NAME).getString();
+            loadSkinForName(customName);
+            skinLocation = skinCache.getOrDefault(customName, Minecraft.getInstance().player.getSkin().texture());
+        } else {
+            skinLocation = Minecraft.getInstance().player.getSkin().texture();
+        }
+
         poseStack.pushPose();
         switch (displayContext) {
             case THIRD_PERSON_RIGHT_HAND:
@@ -88,7 +148,6 @@ public class TotemItemRenderer extends BlockEntityWithoutLevelRenderer {
                 break;
         }
 
-        ResourceLocation skinLocation = customSkin != null ? customSkin : Minecraft.getInstance().player.getSkin().texture();
         VertexConsumer vertexConsumer = buffer.getBuffer(RenderType.entityTranslucent(skinLocation));
 
         this.playerModel.setAllVisible(true);
@@ -100,6 +159,44 @@ public class TotemItemRenderer extends BlockEntityWithoutLevelRenderer {
         playerModel.renderToBuffer(poseStack, vertexConsumer, combinedLight, OverlayTexture.NO_OVERLAY, -1);
 
         poseStack.popPose();
+    }
 
+    private static class MojangUUIDResponse {
+        String id;
+        String name;
+    }
+
+    private static class MojangProfileResponse {
+        String id;
+        String name;
+        Property[] properties;
+
+        String getSkinURL() {
+            for (Property prop : properties) {
+                if (prop.name.equals("textures")) {
+                    String decoded = new String(java.util.Base64.getDecoder().decode(prop.value));
+                    TexturesResponse textures = GSON.fromJson(decoded, TexturesResponse.class);
+                    return textures.textures.SKIN.url;
+                }
+            }
+            return null;
         }
     }
+
+    private static class Property {
+        String name;
+        String value;
+    }
+
+    private static class TexturesResponse {
+        Textures textures;
+
+        private static class Textures {
+            Skin SKIN;
+        }
+
+        private static class Skin {
+            String url;
+        }
+    }
+}
