71122ed341e4cd705ffae4e0bd31f989461d51b1
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-12-20 09:38:18 +0100

    black

--- src/main/java/com/leclowndu93150/playertotem/TotemItemRenderer.java
+++ src/main/java/com/leclowndu93150/playertotem/TotemItemRenderer.java
@@ -2,6 +2,7 @@ package com.leclowndu93150.playertotem;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.EntityModelSet;
@@ -38,15 +39,37 @@ public class TotemItemRenderer extends BlockEntityWithoutLevelRenderer {
         initializeModel();
         poseStack.pushPose();
 
+        switch (displayContext) {
+            case GUI -> {
+                poseStack.translate(0, -3.25 / 16.0F, 0);
+                poseStack.mulPose(Axis.XP.rotationDegrees(30));
+                poseStack.mulPose(Axis.YP.rotationDegrees(225));
+                poseStack.scale(0.5F, 0.5F, 0.5F);
+            }
+            case GROUND -> {
+                poseStack.translate(0, 2 / 16.0F, 0);
+                poseStack.scale(0.5F, 0.5F, 0.5F);
+            }
+            case THIRD_PERSON_RIGHT_HAND, THIRD_PERSON_LEFT_HAND -> {
+                poseStack.translate(0.25 / 16.0F, 0.5 / 16.0F, 1.5 / 16.0F);
+                poseStack.mulPose(Axis.XP.rotationDegrees(0));
+                poseStack.mulPose(Axis.YP.rotationDegrees(90));
+                poseStack.mulPose(Axis.ZP.rotationDegrees(-10));
+                poseStack.scale(0.25F, 0.25F, 0.25F);
+            }
+            case FIRST_PERSON_RIGHT_HAND, FIRST_PERSON_LEFT_HAND -> {
+                poseStack.translate(3 / 16.0F, 0.5 / 16.0F, 1.5 / 16.0F);
+                poseStack.mulPose(Axis.XP.rotationDegrees(0));
+                poseStack.mulPose(Axis.YP.rotationDegrees(90));
+                poseStack.mulPose(Axis.ZP.rotationDegrees(-10));
+                poseStack.scale(0.25F, 0.25F, 0.25F);
+            }
+            default -> {
+                poseStack.scale(0.4F, -0.4F, 0.4F);
+            }
+        }
         LocalPlayer player = Minecraft.getInstance().player;
         if (player != null) {
-            if (displayContext != ItemDisplayContext.GUI) {
-                poseStack.translate(0.5F, 0.5F, 0.5F);
-            }
-
-            float scale = (displayContext == ItemDisplayContext.GUI) ? 0.6F : 0.4F;
-            poseStack.scale(scale, -scale, scale);
-
             ResourceLocation skinLocation = player.getSkin().texture();
             VertexConsumer vertexConsumer = buffer.getBuffer(RenderType.entityTranslucent(skinLocation));
 
@@ -62,4 +85,5 @@ public class TotemItemRenderer extends BlockEntityWithoutLevelRenderer {
 
         poseStack.popPose();
     }
+
 }
