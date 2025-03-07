2d4e1bc23f1a635d56fcc471317c087c844355e5
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-03-07 20:15:41 +0100

    done

--- src/main/java/com/leclowndu93150/myminime/TotemItemRenderer.java
+++ src/main/java/com/leclowndu93150/myminime/TotemItemRenderer.java
@@ -1,21 +1,30 @@
 package com.leclowndu93150.myminime;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.model.geom.ModelLayers;
+import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 
 public class TotemItemRenderer extends BlockEntityWithoutLevelRenderer {
     private ItemStack originalMainHandItem;
     private ItemStack originalOffHandItem;
+
+    // Fields for static model
+    private PlayerModel<AbstractClientPlayer> playerModel;
+    private PlayerModel<AbstractClientPlayer> slimPlayerModel;
 
     // Fields to store rotation values
     private float yBodyRot, yRot, yRotO, yBodyRotO, xRot, xRotO, yHeadRotO, yHeadRot;
@@ -26,6 +35,16 @@ public class TotemItemRenderer extends BlockEntityWithoutLevelRenderer {
         super(Minecraft.getInstance().getBlockEntityRenderDispatcher(), Minecraft.getInstance().getEntityModels());
     }
 
+    private void initializeModels() {
+        if (playerModel == null || slimPlayerModel == null) {
+            ModelPart modelPart = Minecraft.getInstance().getEntityModels().bakeLayer(ModelLayers.PLAYER);
+            ModelPart slimModelPart = Minecraft.getInstance().getEntityModels().bakeLayer(ModelLayers.PLAYER_SLIM);
+
+            this.playerModel = new PlayerModel<>(modelPart, false);
+            this.slimPlayerModel = new PlayerModel<>(slimModelPart, true);
+        }
+    }
+
     @Override
     public void renderByItem(ItemStack stack, ItemDisplayContext displayContext, PoseStack poseStack,
                              MultiBufferSource buffer, int combinedLight, int combinedOverlay) {
@@ -34,30 +53,62 @@ public class TotemItemRenderer extends BlockEntityWithoutLevelRenderer {
 
         try {
             Minecraft mc = Minecraft.getInstance();
-            EntityRenderDispatcher dispatcher = mc.getEntityRenderDispatcher();
-
             AbstractClientPlayer playerToRender = mc.player;
             if (playerToRender == null) return;
 
-            savePlayerEquipment(playerToRender);
-            checkAndReplaceTotemItems(playerToRender, stack);
-            saveAndSetRotations(playerToRender);
-
-            PlayerRenderer renderer = (PlayerRenderer) dispatcher.getRenderer(playerToRender);
-
-            poseStack.pushPose();
-            setupDisplayContextTransformation(displayContext, poseStack, playerToRender);
-
-            float partialTick = mc.getFrameTime();
-
-            renderer.render(playerToRender, 0, partialTick, poseStack, buffer, combinedLight);
-
-            poseStack.popPose();
-            restoreRotations(playerToRender);
-            restorePlayerEquipment(playerToRender);
+            // Use static model only for GUI
+            if (displayContext == ItemDisplayContext.GUI) {
+                renderStaticModelInGui(playerToRender, stack, poseStack, buffer, combinedLight, combinedOverlay);
+            } else {
+                renderEntityModel(playerToRender, stack, displayContext, poseStack, buffer, combinedLight, combinedOverlay);
+            }
         } finally {
             isRenderingTotem = false;
         }
+    }
+
+    private void renderStaticModelInGui(AbstractClientPlayer player, ItemStack stack, PoseStack poseStack,
+                                        MultiBufferSource buffer, int combinedLight, int combinedOverlay) {
+        initializeModels();
+
+        boolean isSlimModel = player.getModelName().equals("slim");
+        PlayerModel<AbstractClientPlayer> modelToUse = isSlimModel ? slimPlayerModel : playerModel;
+
+        poseStack.pushPose();
+
+        poseStack.translate(0.5D, 0.75D, 0D);
+        poseStack.mulPose(Axis.XP.rotationDegrees(-180f));
+        poseStack.scale(0.5F, 0.5F, 0.49F);
+
+        VertexConsumer vertexConsumer = buffer.getBuffer(RenderType.entityTranslucent(player.getSkinTextureLocation()));
+        modelToUse.setAllVisible(true);
+        modelToUse.young = false;
+
+        modelToUse.renderToBuffer(poseStack, vertexConsumer, combinedLight, OverlayTexture.NO_OVERLAY, 1, 1, 1, 1);
+
+        poseStack.popPose();
+    }
+
+    private void renderEntityModel(AbstractClientPlayer playerToRender, ItemStack stack, ItemDisplayContext displayContext,
+                                   PoseStack poseStack, MultiBufferSource buffer, int combinedLight, int combinedOverlay) {
+        EntityRenderDispatcher dispatcher = Minecraft.getInstance().getEntityRenderDispatcher();
+
+        savePlayerEquipment(playerToRender);
+        checkAndReplaceTotemItems(playerToRender, stack);
+        saveAndSetRotations(playerToRender);
+
+        PlayerRenderer renderer = (PlayerRenderer) dispatcher.getRenderer(playerToRender);
+
+        poseStack.pushPose();
+        setupDisplayContextTransformation(displayContext, poseStack, playerToRender);
+
+        float partialTick = Minecraft.getInstance().getFrameTime();
+
+        renderer.render(playerToRender, 0, partialTick, poseStack, buffer, combinedLight);
+
+        poseStack.popPose();
+        restoreRotations(playerToRender);
+        restorePlayerEquipment(playerToRender);
     }
 
     private void saveAndSetRotations(AbstractClientPlayer player) {
@@ -176,11 +227,6 @@ public class TotemItemRenderer extends BlockEntityWithoutLevelRenderer {
             case GROUND -> {
                 poseStack.translate(0.5D, 0.3D, 0.5D);
                 poseStack.scale(0.25F, 0.25F, 0.25F);
-            }
-            case GUI -> {
-                poseStack.translate(0.5D, 0D, 0D);
-                poseStack.mulPose(Axis.YP.rotationDegrees(-180f));
-                poseStack.scale(0.5F, 0.5F, 0.49F);
             }
             case FIXED -> {
                 poseStack.translate(0.5D, 0D, 0.45D);
